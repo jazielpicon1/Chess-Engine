79af9abd48030ea734c18998a62f3c2bc1ff2f22
jazielpicon1 <64537736+jazielpicon1@users.noreply.github.com>
2024-11-21 01:21:09 -0500

    Add files via upload

---
+++ src/main/main.java
@@ -0,0 +1,20 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.getContentPane().setBackground(new Color(128, 128, 128));
+        frame.setLayout(new GridBagLayout());
+        frame.setMinimumSize(new Dimension(1000, 1000));
+        frame.setLocationRelativeTo(null);
+
+        Board board = new Board();
+        frame.add(board);
+
+        frame.setVisible(true);
+
+    }
+}
